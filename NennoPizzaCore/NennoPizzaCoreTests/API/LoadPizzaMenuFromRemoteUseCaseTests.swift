b2218e281c4b8f5f818b88046a7a3402b5e009e0
Vadym Bohdan <vadymbog@gmail.com>
2023-07-02 17:05:47 +0300

    `RemotePizzaMenuLoader` doesn't request data from URL on init

---
+++ NennoPizzaCore/NennoPizzaCoreTests/API/LoadPizzaMenuFromRemoteUseCaseTests.swift
@@ -0,0 +1,76 @@
+//
+//  LoadPizzaMenuFromRemoteUseCaseTests.swift
+//  NennoPizzaCoreTests
+//
+//  Created by Vadym Bohdan on 02.07.2023.
+//
+
+import XCTest
+import NennoPizzaCore
+
+struct PizzaMenu {
+    let pizzas: [Pizza]
+    let basePrice: Double
+}
+
+struct Pizza {
+}
+
+protocol HTTPClient {
+    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
+    
+    func get(from url: URL, completion: @escaping (Result) -> Void)
+}
+
+protocol PizzaMenuLoader {
+    typealias Result = Swift.Result<PizzaMenu, Error>
+    
+    func load(completion: @escaping (Result) -> Void)
+}
+
+class RemotePizzaMenuLoader: PizzaMenuLoader {
+    typealias Result = PizzaMenuLoader.Result
+    
+    init(url: URL, client: HTTPClient) {
+        
+    }
+    
+    func load(completion: @escaping (Result) -> Void) {}
+}
+
+final class LoadPizzaMenuFromRemoteUseCaseTests: XCTestCase {
+    
+    func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "https://any-url.com")!
+        let client = HTTPClientSpy()
+        let _ = RemotePizzaMenuLoader(url: url, client: client)
+        
+        XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    
+    class HTTPClientSpy: HTTPClient {
+        private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
+        
+        var requestedURLs: [URL] {
+            return messages.map { $0.url }
+        }
+        
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+            messages.append((url, completion))
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            messages[index].completion(.failure(error))
+        }
+        
+        func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
+            let response = HTTPURLResponse(
+                url: requestedURLs[index],
+                statusCode: code,
+                httpVersion: nil,
+                headerFields: nil
+            )!
+            messages[index].completion(.success((data, response)))
+        }
+    }
+}
