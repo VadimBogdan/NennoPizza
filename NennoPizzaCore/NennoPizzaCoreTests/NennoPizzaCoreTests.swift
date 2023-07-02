0dfecad2d24eec260bbe2ac2e306b957f0cf9b28
Vadym Bohdan <vadymbog@gmail.com>
2023-07-02 16:43:47 +0300

    Create new project `NennoPizzaCore` for application business logic.

---
+++ NennoPizzaCore/NennoPizzaCoreTests/NennoPizzaCoreTests.swift
@@ -0,0 +1,13 @@
+//
+//  NennoPizzaCoreTests.swift
+//  NennoPizzaCoreTests
+//
+//  Created by Vadym Bohdan on 02.07.2023.
+//
+
+import XCTest
+import NennoPizzaCore
+
+final class NennoPizzaCoreTests: XCTestCase {
+    
+}
