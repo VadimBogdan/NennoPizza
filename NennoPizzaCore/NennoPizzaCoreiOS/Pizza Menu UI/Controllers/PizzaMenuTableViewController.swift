3341b1316a4a3cb582a28d4e4c2b43d9afa80fe7
Vadym Bohdan <vadymbog@gmail.com>
2023-07-03 19:20:07 +0300

    Add `PizzaMenuTableViewController`

---
+++ NennoPizzaCore/NennoPizzaCoreiOS/Pizza Menu UI/Controllers/PizzaMenuTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PizzaMenuTableViewController.swift
+//  NennoPizzaCoreiOS
+//
+//  Created by Vadym Bohdan on 03.07.2023.
+//
+
+import UIKit
+
+final class PizzaMenuTableViewController: UITableViewController {
+    
+}
