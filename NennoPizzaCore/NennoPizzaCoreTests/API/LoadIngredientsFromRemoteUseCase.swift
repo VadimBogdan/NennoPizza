eefc7bd92000953ed933e18f5c4b74cb6421ef06
Vadym Bohdan <vadymbog@gmail.com>
2023-07-03 14:20:44 +0300

    `load` delivers error on non 200 HTTP response

--- NennoPizzaCore/NennoPizzaCoreTests/API/LoadIngredientsFromRemoteUseCase.swift
+++ NennoPizzaCore/NennoPizzaCoreTests/API/LoadIngredientsFromRemoteUseCase.swift
@@ -14,6 +14,12 @@ struct Ingredient: Equatable {
     let id: Int
 }
 
+struct RemoteIngredient: Decodable {
+    let price: Double
+    let name: String
+    let id: Int
+}
+
 protocol IngredientsLoader {
     typealias Result = Swift.Result<[Ingredient], Error>
     
@@ -39,10 +45,48 @@ class RemoteIngredientsLoader: IngredientsLoader {
     func load(completion: @escaping (Result) -> Void) {
         client.get(from: url) { [weak self] result in
             guard self != nil else { return }
-            if case .failure = result {
+            
+            switch result {
+            case let .success((data, response)):
+                completion(RemoteIngredientsLoader.map(data, from: response))
+                
+            case .failure:
                 completion(.failure(Error.connectivity))
             }
         }
+    }
+    
+    private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
+        do {
+            let ingredients = try IngredientsMapper.map(data, from: response)
+            return .success(ingredients.toModels())
+        } catch {
+            return .failure(error)
+        }
+    }
+}
+
+extension HTTPURLResponse {
+    private static var OK_200: Int { return 200 }
+    
+    var isOK: Bool {
+        return statusCode == HTTPURLResponse.OK_200
+    }
+}
+
+final class IngredientsMapper {
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteIngredient] {
+        guard response.isOK, let remotePizzaMenu = try? JSONDecoder().decode([RemoteIngredient].self, from: data) else {
+            throw RemoteIngredientsLoader.Error.invalidData
+        }
+        
+        return remotePizzaMenu
+    }
+}
+
+private extension Array where Element == RemoteIngredient {
+    func toModels() -> [Ingredient] {
+        map { Ingredient(price: $0.price, name: $0.name, id: $0.id) }
     }
 }
 
@@ -82,6 +126,19 @@ final class LoadIngredientsFromRemoteUseCase: XCTestCase {
         })
     }
     
+    func test_load_deliversErrorOnNon200HTTPResponse() {
+        let (sut, client) = makeSUT()
+        
+        let samples = [199, 201, 300, 400, 500]
+        
+        samples.enumerated().forEach { index, code in
+            expect(sut, toCompleteWith: failure(.invalidData), when: {
+                let json = makeJSONData([])
+                client.complete(withStatusCode: code, data: json, at: index)
+            })
+        }
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://any-url.com")!, file: StaticString = #file, line: UInt = #line) -> (sut: RemoteIngredientsLoader, client: HTTPClientSpy) {
@@ -94,6 +151,10 @@ final class LoadIngredientsFromRemoteUseCase: XCTestCase {
     
     private func failure(_ error: RemoteIngredientsLoader.Error) -> RemoteIngredientsLoader.Result {
         .failure(error)
+    }
+    
+    private func makeJSONData(_ json: [[String: Any]]) -> Data {
+        return try! JSONSerialization.data(withJSONObject: json)
     }
     
     private func expect(_ sut: RemoteIngredientsLoader, toCompleteWith expectedResult: RemoteIngredientsLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
