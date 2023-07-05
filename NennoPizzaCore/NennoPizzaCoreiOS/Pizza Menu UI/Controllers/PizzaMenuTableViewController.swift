5e4e9cb1e8952ac8856f4fc7fba9de7eaa13756a
Vadym Bohdan <vadymbog@gmail.com>
2023-07-05 11:26:02 +0300

    Move `PizzaCellController` to a separate file

--- NennoPizzaCore/NennoPizzaCoreiOS/Pizza Menu UI/Controllers/PizzaMenuTableViewController.swift
+++ NennoPizzaCore/NennoPizzaCoreiOS/Pizza Menu UI/Controllers/PizzaMenuTableViewController.swift
@@ -8,31 +8,6 @@
 import UIKit
 import NennoPizzaCore
 
-public final class PizzaCellController: PizzaView {
-    
-    private var cell: PizzaMenuTableViewCell?
-    
-    func view(in tableView: UITableView, for indexPath: IndexPath) -> UITableViewCell {
-        cell = tableView.dequeueReusableCell(for: indexPath)
-        return cell!
-    }
-    
-    public func display(_ model: PizzaViewModel<UIImage>) {
-        cell?.pizzaIngredientsLabel.text = model.ingredients
-        cell?.pizzaNameLabel.text = model.name
-        cell?.pizzaPriceLabel.text = model.price
-        cell?.pizzaImageView.image = model.image
-
-        cell?.onReuse = { [weak self] in
-            self?.releaseCellForReuse()
-        }
-    }
-    
-    private func releaseCellForReuse() {
-        cell = nil
-    }
-}
-
 enum DesignConstants {
     static let pizzaMenuCellHeight: CGFloat = 178
 }
