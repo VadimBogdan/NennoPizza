276e9857e216346a4b4c75d11872a7aed5a7c2ed
Vadym Bohdan <vadymbog@gmail.com>
2023-07-08 22:50:34 +0300

    Implement `CartUIComposer`

---
+++ NennoPizza/Composition/Cart/CartUIComposer.swift
@@ -0,0 +1,43 @@
+//
+//  CartUIComposer.swift
+//  NennoPizza
+//
+//  Created by Vadym Bohdan on 08.07.2023.
+//
+
+import NennoPizzaCoreiOS
+import NennoPizzaCore
+import Foundation
+
+class CartUIComposer {
+    
+    private init() {}
+    
+    public static func cartComposedWith(cartLoader: @escaping () -> Cart) -> CartViewController {
+        let presentationAdapter = CartPresentationAdapter(
+            cartLoader: cartLoader,
+            didSelectCheckoutCallBack: {
+                
+            })
+        
+        let cartViewController = makeCartViewController(delegate: presentationAdapter,
+                                                        title: CartPresenter.title)
+        
+        let adapter = CartViewAdapter(controller: cartViewController)
+        
+        let presenter = CartPresenter(cartView: adapter,
+                                      cartTotalPriceView: WeakRefVirtualProxy(adapter), priceFormatter: { NumberFormatter.format($0, currency: "$") })
+        
+        presentationAdapter.presenter = presenter
+        
+        return cartViewController
+    }
+    
+    private static func makeCartViewController(delegate: CartViewControllerDelegate, title: String) -> CartViewController {
+        let cartController = CartViewController()
+        cartController.delegate = delegate
+        cartController.title = title
+        return cartController
+    }
+    
+}
