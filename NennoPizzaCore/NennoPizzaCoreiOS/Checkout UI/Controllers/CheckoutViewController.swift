441e54c64676a9f364e7fdb07ae350da481f8ee4
Vadym Bohdan <vadymbog@gmail.com>
2023-07-09 22:00:00 +0300

    Create `CheckoutViewController`

---
+++ NennoPizzaCore/NennoPizzaCoreiOS/Checkout UI/Controllers/CheckoutViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CheckoutViewController.swift
+//  NennoPizzaCoreiOS
+//
+//  Created by Vadym Bohdan on 09.07.2023.
+//
+
+import UIKit
+
+class CheckoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
