3725154391a31e04c2504f13a3a5aadca7e08a26
Vadym Bohdan <vadymbog@gmail.com>
2023-07-10 18:29:16 +0300

    Create `DrinksViewController`

---
+++ NennoPizzaCore/NennoPizzaCoreiOS/Drinks UI/Controllers/DrinksViewController.swift
@@ -0,0 +1,12 @@
+//
+//  DrinksViewController.swift
+//  NennoPizzaCoreiOS
+//
+//  Created by Vadym Bohdan on 10.07.2023.
+//
+
+import UIKit
+
+final class DrinksViewController: UIViewController {
+    
+}
