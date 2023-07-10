15fe22f6c8908734b37f2ac9935ac1944806a103
Vadym Bohdan <vadymbog@gmail.com>
2023-07-10 11:31:22 +0300

    Add presentation logic to `CheckoutViewController`

--- NennoPizzaCore/NennoPizzaCoreiOS/Checkout UI/Controllers/CheckoutViewController.swift
+++ NennoPizzaCore/NennoPizzaCoreiOS/Checkout UI/Controllers/CheckoutViewController.swift
@@ -6,16 +6,25 @@
 //
 
 import UIKit
+import NennoPizzaCore
 
-class CheckoutViewController: UIViewController {
+public protocol CheckoutViewControllerDelegate {
+    func didRequestCheckout()
+}
+
+public final class CheckoutViewController: UIViewController, CheckoutView {
     
     private let messageLabel = UILabel()
     private let footerView = UIView()
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
         
         setup()
+    }
+    
+    public func display(_ viewModel: CheckoutViewModel) {
+        messageLabel.text = viewModel.checkoutMessage
     }
     
     private func setup() {
@@ -25,6 +34,9 @@ class CheckoutViewController: UIViewController {
         footerView.backgroundColor = .redAttention
         messageLabel.font = .italicSystemFont(ofSize: 34)
         messageLabel.textColor = .redSecondary
+        
+        view.addSubview(messageLabel)
+        view.addSubview(footerView)
         
         NSLayoutConstraint.activate([
             messageLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
