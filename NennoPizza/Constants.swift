01268957865bd1c438231e7d8c8e5c64786b9079
Vadym Bohdan <vadymbog@gmail.com>
2023-07-11 13:29:35 +0300

    Create Constants enum with often-used constants

---
+++ NennoPizza/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  NennoPizza
+//
+//  Created by Vadym Bohdan on 11.07.2023.
+//
+
+import Foundation
+
+enum Constants {
+    static let addedToCartViewDuration: TimeInterval = 3.0
+    static let usdCurrencySymbol = "$"
+}
